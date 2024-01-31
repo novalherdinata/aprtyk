d72d87c54b71b0dd3e2e23c984944fbab800618a
novalekaherdinata <novalekaherdinata@gmail.com>
2024-01-31 10:31:08 +0700

    remove git

--- Main.py
+++ Main.py
@@ -51,6 +51,19 @@ st.set_page_config(
     }
 )
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.header('Deteksi Plat Nomor Menggunakan Yolo & Tesseract OCR')      
 st.subheader('By Rahmat Kurniawan', divider='rainbow')  
 with st.sidebar:
